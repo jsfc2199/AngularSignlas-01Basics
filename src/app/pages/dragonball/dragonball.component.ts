060ab72c3e1de8885382624787493cf93d15981f
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-03-24 11:53:39 -0500

    css dinamico en funcion del caracter

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -3,7 +3,7 @@ import { Component, signal } from '@angular/core';
 interface Character {
   id: number;
   name: string;
-  power: number
+  power: number;
 }
 @Component({
   selector: 'app-dragonball',
@@ -11,10 +11,9 @@ interface Character {
   templateUrl: './dragonball.component.html',
 })
 export class DragonballComponent {
-
   characters = signal<Character[]>([
-    {id:1, name: 'goku', power: 100},
-    {id:2, name: 'vegeta', power: 100},
-    {id:3, name: 'trunks', power: 100},
-  ])
+    { id: 1, name: 'goku', power: 200 },
+    { id: 2, name: 'vegeta', power: 100 },
+    { id: 3, name: 'trunks', power: 100 },
+  ]);
 }
