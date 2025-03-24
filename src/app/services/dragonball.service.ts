32a820a96897a4d915d23c6739329841207a7051
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-03-24 13:28:51 -0500

    cargar del local storage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,15 +1,16 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../pages/dragonball/dragonball.component';
 
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters')
+  return characters ? JSON.parse(characters) : []
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DragonballService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'goku', power: 200 },
-    { id: 2, name: 'vegeta', power: 100 },
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
 
   //efectos para disparar cosas secundarias, queremos un efecto que se ejecute cada que cambie los characters, esto no es necesario invocarlo en ninguna parte
   //es por asi decirlo autoinvocado porque es un efecto
