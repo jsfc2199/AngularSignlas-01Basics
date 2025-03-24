2f8918b7d471488d6f5fa3b26057366b49a1b1e7
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-03-24 12:20:06 -0500

    agregar un personaje desde el formulario usando señales

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -11,11 +11,34 @@ interface Character {
   templateUrl: './dragonball.component.html',
 })
 export class DragonballComponent {
+  name = signal('gohan');
+  power = signal(0);
+
   characters = signal<Character[]>([
     { id: 1, name: 'goku', power: 200 },
     { id: 2, name: 'vegeta', power: 100 },
     { id: 3, name: 'trunks', power: 100 },
     { id: 4, name: 'piccolo', power: 20 },
-
   ]);
+
+  addCharater() {
+    if (!this.power() || !this.name()) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    //actualizamos la señal
+    this.characters.update((list) => [...list, newCharacter]);
+
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set(''), this.power.set(0);
+  }
 }
