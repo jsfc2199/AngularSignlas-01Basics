6f0c48620b3514d14aed9a1174ff0f21b00ade82
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-03-24 13:25:16 -0500

    uso de effect para guardar en localStorage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../pages/dragonball/dragonball.component';
 
 @Injectable({
@@ -10,6 +10,12 @@ export class DragonballService {
     { id: 1, name: 'goku', power: 200 },
     { id: 2, name: 'vegeta', power: 100 },
   ]);
+
+  //efectos para disparar cosas secundarias, queremos un efecto que se ejecute cada que cambie los characters, esto no es necesario invocarlo en ninguna parte
+  //es por asi decirlo autoinvocado porque es un efecto
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
+  })
 
   addCharacter(character: Character){
     this.characters.update(list => [...list, character])
