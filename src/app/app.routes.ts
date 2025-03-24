2559db8e78ebb1edda8cc0dd3202ff4c063309aa
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-03-24 11:34:19 -0500

    preparacion pagina para dragonball page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter/counter.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { DragonballComponent } from './pages/dragonball/dragonball.component';
 
 export const routes: Routes = [
   { path: '', component: CounterComponent },
   {
     path: 'hero',
     component: HeroComponent,
+  },
+  {
+    path: 'dragonball',
+    component: DragonballComponent,
   },
 ];
